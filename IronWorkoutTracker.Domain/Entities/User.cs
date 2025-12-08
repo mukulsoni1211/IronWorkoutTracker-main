5bc502662218d4037d81e4bd940d08031f5189bf
mukulsoni <mukulsoni1211@gmail.com>
2025-12-09 00:07:49 +0530

    updated user and implement migration

--- IronWorkoutTracker.Domain/Entities/User.cs
+++ IronWorkoutTracker.Domain/Entities/User.cs
@@ -5,8 +5,10 @@ namespace IronWorkoutTracker.Domain.Entities;
 public class User
 {
  public int Id { get; set; }
- public string Name { get; set; }
+ public string? Name { get; set; }
  public string Email { get; set; }
  public string Password { get; set; }
  public string Role { get; set; }
+ public bool? IsActive { get; set; }
+ public DateTime? CreatedDate { get; set; }
 }
---
+++ IronWorkoutTracker.Infrastructure/Migrations/20251208183050_InitialUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IronWorkoutTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
